f05945332b6450ce0f33c5e10d501dd0f1aa63ce
Mikhail Fursov <mike.fursov@gmail.com>
2025-12-21 11:52:02 -0600

    Update eslint & squid packages

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+const ts = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const unusedImports = require('eslint-plugin-unused-imports');
+
+module.exports = [
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/*.js'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'after-used',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          caughtErrors: 'all',
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'eqeqeq': 'error',
+    },
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+];
